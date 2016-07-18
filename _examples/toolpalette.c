5ba160c200f89c60185b4f698dbf73693ebf717e
Andrew Steele <andrew@andrewsteele.me.uk>
2016-07-18 11:24:40 +0100

    Added methods for ToolItemGroup container Change ToolPalette example removing deprecations

--- _examples/toolpalette.c
+++ _examples/toolpalette.c
@@ -6,42 +6,49 @@ static void destroy(GtkWidget *widget, gpointer data)
 }
 
 int main(int argc, char *argv[])
-{    
+{
     gtk_init(&argc, &argv);
-    
+
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "ToolPalette");
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
     g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
-    
+
     GtkWidget *toolpalette = gtk_tool_palette_new();
     gtk_container_add(GTK_CONTAINER(window), toolpalette);
-    
+
     GtkWidget *toolitemgroup;
     GtkToolItem *toolitem;
-    
+
     toolitemgroup = gtk_tool_item_group_new("Group 1");
     gtk_container_add(GTK_CONTAINER(toolpalette), toolitemgroup);
-    
-    toolitem = gtk_tool_button_new_from_stock(GTK_STOCK_HOME);
+
+    toolitem = gtk_tool_button_new(NULL, "Home");
+    gtk_tool_button_set_icon_name(GTK_TOOL_BUTTON(toolitem), "gtk-home");
     gtk_tool_item_group_insert(GTK_TOOL_ITEM_GROUP(toolitemgroup), toolitem, -1);
-    toolitem = gtk_tool_button_new_from_stock(GTK_STOCK_ABOUT);
+    toolitem = gtk_tool_button_new(NULL, "About");
+    gtk_tool_button_set_icon_name(GTK_TOOL_BUTTON(toolitem), "gtk-about");
     gtk_tool_item_group_insert(GTK_TOOL_ITEM_GROUP(toolitemgroup), toolitem, -1);
-    toolitem = gtk_tool_button_new_from_stock(GTK_STOCK_HELP);
+    toolitem = gtk_tool_button_new(NULL, "Help");
+    gtk_tool_button_set_icon_name(GTK_TOOL_BUTTON(toolitem), "gtk-help");
     gtk_tool_item_group_insert(GTK_TOOL_ITEM_GROUP(toolitemgroup), toolitem, -1);
-    
+
     toolitemgroup = gtk_tool_item_group_new("Group 2");
     gtk_container_add(GTK_CONTAINER(toolpalette), toolitemgroup);
-    
-    toolitem = gtk_tool_button_new_from_stock(GTK_STOCK_ADD);
+
+    toolitem = gtk_tool_button_new(NULL, "Add");
+    gtk_tool_button_set_icon_name(GTK_TOOL_BUTTON(toolitem), "gtk-add");
     gtk_tool_item_group_insert(GTK_TOOL_ITEM_GROUP(toolitemgroup), toolitem, -1);
-    toolitem = gtk_tool_button_new_from_stock(GTK_STOCK_EDIT);
+    toolitem = gtk_tool_button_new(NULL, "Edit");
+    gtk_tool_button_set_icon_name(GTK_TOOL_BUTTON(toolitem), "gtk-edit");
     gtk_tool_item_group_insert(GTK_TOOL_ITEM_GROUP(toolitemgroup), toolitem, -1);
-    toolitem = gtk_tool_button_new_from_stock(GTK_STOCK_DELETE);
+    toolitem = gtk_tool_button_new(NULL, "Delete");
+    gtk_tool_button_set_icon_name(GTK_TOOL_BUTTON(toolitem), "gtk-delete");
     gtk_tool_item_group_insert(GTK_TOOL_ITEM_GROUP(toolitemgroup), toolitem, -1);
-    
+
     gtk_widget_show_all(window);
-    
+
     gtk_main();
-    
+
     return 0;
 }
--- toolitemgroup.rst
+++ toolitemgroup.rst
@@ -7,4 +7,52 @@ Constructor
 ===========
 The constructor for the ToolItemGroup is::
 
-  GtkWidget *toolitemgroup = gtk_tool_item_group_new();
+  GtkWidget *toolitemgroup = gtk_tool_item_group_new(label);
+
+A *label* string should be supplied which is displayed on the expander portion of the ToolItemGroup. Its use is to provide a section header for the contained ToolItem widgets.
+
+=======
+Methods
+=======
+A label can be changed after construction with::
+
+  gtk_tool_item_group_set_label(GTK_TOOL_ITEM_GROUP(toolitemgroup), label);
+
+A :doc:`label` widget can be used instead of a string via the method::
+
+  gtk_tool_item_group_set_label_widget(GTK_TOOL_ITEM_GROUP(toolitemgroup), label);
+
+Items can be added to the ToolItemGroup with the method::
+
+  gtk_tool_item_group_insert(GTK_TOOL_ITEM_GROUP(toolitemgroup), toolitem, position);
+
+The *toolitem* parameter specifies the ToolItem to be added to the group. The *position* takes an integer value which specifies where in the ToolPalette the group will be added, with ``0`` used for the first position.
+
+A ToolItem can also be repositioned within the group via::
+
+  gtk_tool_item_group_set_item_position(GTK_TOOL_ITEM_GROUP(toolitemgroup), toolitem, position);
+
+An items position within the ToolItemGroup can be retrieved by running::
+
+  gtk_tool_item_group_get_item_position(GTK_TOOL_ITEM_GROUP(toolitemgroup), toolitem);
+
+Alternatively, an item within a ToolItemGroup can be retrieved using its position index with::
+
+  gtk_tool_item_group_get_nth_item(GTK_TOOL_ITEM_GROUP(toolitemgroup), position);
+
+If required, items can be removed by calling::
+
+  gtk_tool_item_group_remove(GTK_TOOL_ITEM_GROUP(toolitemgroup), toolitem);
+
+By default, all ToolItemGroup objects are set as expanded and all icons visible. However, it may be necessary in some cases to collapse some groups programatically::
+
+  gtk_tool_item_group_set_collapsed(GTK_TOOL_ITEM_GROUP(toolitemgroup), collapsed);
+
+The number of ToolItem widgets contained within the ToolItemGroup can be obtained from::
+
+  gtk_tool_item_group_get_n_items(GTK_TOOL_ITEM_GROUP(toolitemgroup));
+
+=======
+Example
+=======
+For an example of the ToolItemGroup, see the :doc:`toolpalette` page.
